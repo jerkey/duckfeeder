8ce11e9f489464f9c5bd028a316f7ef5793d1e0c
jerkey <jerkey@halo.nu>
2014-08-02 15:04:49 -0700

    duckfeeder opens and closes, slowly, in response to o or c commands

--- duckfeeder.ino
+++ duckfeeder.ino
@@ -1,136 +1,72 @@
-#define STEPPIN 11     
-#define DIRPIN 12      
-#define STEPSPERMM 15.87 // 1000 steps = 63mm of wire.
-#define WIRELENGTH 77 * STEPSPERMM // 77mm total pieces wanted.
-#define STRIPLENGTH 4 * STEPSPERMM // 5mm stripped
-#define TOOLOFFSET 9.5 * STEPSPERMM // 9.5mm is distance between stripper and chopper
-#define FWD HIGH // which way is DIR wired?
-#define BACK LOW
-#define STRIPOPEN 20 // position of stripper servo
-#define STRIPCLOSED 175
-#define CHOPCLOSED 170
-#define CHOPOPEN 10
-#define STRIPPIN 9 // what pin is it on
-#define CHOPPIN 10
+#define DOORCLOSED 7 // angle when closed
+#define DOOROPEN 180 // angle when open
+#define DOORPIN 10 // pin for servo
 
-#include <Servo.h> 
-Servo chopServo;  // create servo object to control a servo 
-Servo stripServo;  // create servo object to control a servo 
+#include <Servo.h>
+Servo doorServo;  // create servo object to control a servo
 
 int inByte = 0;         // incoming serial byte
-int speed = 10000;  // how many microseconds delay between steps
+int doorSpeed = 100;  // how many milliseconds delay between degrees
+
+byte position,lastPosition = DOOROPEN;
 
 void setup() {
-  pinMode(STEPPIN, OUTPUT);      
-  pinMode(DIRPIN, OUTPUT);
-  pinMode(CHOPPIN,OUTPUT);
-  chopServo.attach(CHOPPIN);
-  pinMode(STRIPPIN,OUTPUT);
-  stripServo.attach(STRIPPIN);
+  pinMode(DOORPIN,OUTPUT);
+  doorServo.attach(DOORPIN);
   Serial.begin(9600);
-  Serial.println("hello");
-  chopServo.write(CHOPOPEN);
-  stripper(STRIPOPEN);
+  Serial.println("hello i'm a duck food bin");
+  doorServo.write(DOOROPEN);
 }
 
 void loop()
 {
-  // if we get a valid byte, read analog ins:
   if (Serial.available() > 0) {
-    // get incoming byte:
     inByte = Serial.read();
     switch (inByte) {
-    case 'w':
-      Serial.println("wirefeed!");
-      stepper(FWD,WIRELENGTH,speed);
-      break;
-    case 'f':
-      Serial.println("forward");
-      stepper(FWD,STRIPLENGTH,speed);
-      break;
-    case 'b':
-      Serial.println("back");
-      stepper(BACK,STRIPLENGTH,speed);
-      break;
-    case ']':
-      Serial.println("] ooch forward");
-      stepper(FWD,35,speed);
-      break;
-    case '[':
-      Serial.println("[ ooch back");
-      stepper(BACK,35,speed);
-      break;
-    case 'F':
-      Serial.println("forward TOOLOFFSET");
-      stepper(FWD,TOOLOFFSET,speed);
-      break;
-    case 'B':
-      Serial.println("back TOOLOFFSET");
-      stepper(BACK,TOOLOFFSET,speed);
-      break;
     case 'o':
       Serial.println("open");
-      stripper(STRIPOPEN);
+      position = DOOROPEN;
+      Serial.println(position);
+      door(position);
       break;
     case 'c':
       Serial.println("close");
-      stripper(STRIPCLOSED);
+      position = DOORCLOSED;
+      Serial.println(position);
+      door(position);
       break;
-    case 'x':
-      Serial.println("xchop!");
-      stepper(FWD,TOOLOFFSET,speed);
-      chop();
-      stepper(BACK,TOOLOFFSET,speed);
-      break;
-    case 'j':
-      Serial.println("just chop");
-      chop();
-      break;
-    case 'G':
-      Serial.println("Go fwd until any key");
-      while (Serial.available() == 0) stepper(FWD,STRIPLENGTH,speed);
-      inByte = Serial.read();
-      break;
-    case 'R':
-      Serial.println("Reverse until any key");
-      while (Serial.available() == 0) stepper(BACK,STRIPLENGTH,speed);
-      inByte = Serial.read();
-      break;
-    case '=':
-      Serial.print("speed ");
-      speed += 100;
-      Serial.println(speed);
+    case '+':
+      position += 1;
+      Serial.println(position);
+      door(position);
       break;
     case '-':
-      Serial.print("speed ");
-      speed -= 100;
-      Serial.println(speed);
+      position -= 1;
+      Serial.println(position);
+      door(position);
       break;
     default:
-      Serial.println("wirefeed forward back Forward(TOOLOFFSET) Back(TOOLOFFSET) open close xchop justchop Go Reverse");
+      Serial.print(inByte);
+      Serial.println("o for open, c for close");
       break;
     }
   }
 }
 
 void stepper(boolean dir, unsigned int distance, int speed) {
-  digitalWrite(DIRPIN, dir);
   for(int i=0; i <distance ;  i++){
-    digitalWrite(STEPPIN, HIGH);
     delayMicroseconds(500);
-    digitalWrite(STEPPIN, LOW);
     delayMicroseconds(speed);
   }
 }
 
-void stripper(int angle) {
-  stripServo.write(angle);              // tell servo to go to position in variable 'angle'
-  delay(750);                       // waits one second for the servo to reach the position 
-}
-
-void chop() {
-  chopServo.write(CHOPCLOSED);
-  delay(900);
-  chopServo.write(CHOPOPEN);
-  delay(700);
+void door(int angle) {
+  int step = 1; // must be 1 for "i != angle" to work
+  if (angle < lastPosition) step *= -1; // count the right direction
+  for(int i=lastPosition; i != angle;  i += step){
+    doorServo.write(i);
+    delay(doorSpeed);
+  }
+  doorServo.write(angle);
+  lastPosition = angle;
 }
